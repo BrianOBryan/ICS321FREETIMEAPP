55f6407ecc770a47a71dcd92a249d7c283067c73
Brian Mayeshiro <brianmay@hawaii.edu>
2016-05-05 23:23:24 -1000

    added admin.php and site analytics for profile.php

---
+++ admin.php
@@ -0,0 +1,5 @@
+<?php>
+/*redirect to analytics page */
+    header('Location: https://analytics.google.com/analytics/web/?authuser=0#dashboard/default/a77391723w116235567p121549682/');
+    exit();
+?>
